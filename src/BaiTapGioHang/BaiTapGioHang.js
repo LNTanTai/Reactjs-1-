4ae8b87ae502b4258f29443569e5dc4c238f49c0
tai <tailainguyentan@gmail.com>
2020-10-10 16:36:36 +0700

    alo

--- src/BaiTapGioHang/BaiTapGioHang.js
+++ src/BaiTapGioHang/BaiTapGioHang.js
@@ -13,8 +13,13 @@
 
 import React, { Component } from "react";
 import SanPham from "./SanPham";
-
+import Modal from "./Modal";
 export default class BaiTapGioHang extends Component {
+  handleDetail = (sanpham) => {
+    this.setState({
+      sanPhamChiTiet: sanpham,
+    });
+  };
   danhSachSanPham = [
     {
       maSanPham: "1",
@@ -26,6 +31,7 @@ export default class BaiTapGioHang extends Component {
       cameraSau: "Chính 48 MP & Phụ 8 MP, 5 MP",
       ram: "4 GB",
       rom: "6 GB",
+      giaBan: "100000000",
     },
     {
       maSanPham: "2",
@@ -37,6 +43,7 @@ export default class BaiTapGioHang extends Component {
       cameraSau: "Chính 40 MP & Phụ 8 MP, 5 MP",
       ram: "10 GB",
       rom: "12 GB",
+      giaBan: "2300000000",
     },
     {
       maSanPham: "3",
@@ -48,9 +55,9 @@ export default class BaiTapGioHang extends Component {
       cameraSau: "Chính 43 MP & Phụ 4 MP, 5 MP",
       ram: "5 GB",
       rom: "20 GB",
+      giaBan: "765000000",
     },
   ];
-
   state = {
     sanPhamChiTiet: {
       maSanPham: "1",
@@ -62,13 +69,34 @@ export default class BaiTapGioHang extends Component {
       ram: "4 GB",
       rom: "6 GB",
     },
+    danhSachGioHang: [],
+  };
+
+  handleAddSP = (sanPham) => {
+    // console.log("sanPham : ", sanPham);
+    let danhSachGioHang = [...this.state.danhSachGioHang];
+    // danhSachGioHang.push(sanPham);
+    danhSachGioHang = [...danhSachGioHang, sanPham];
+    //setState
+    this.setState(
+      {
+        danhSachGioHang: danhSachGioHang,
+      },
+      () => {
+        console.log(this.state.danhSachGioHang);
+      }
+    );
   };
 
   renderDanhSachSanPham = () => {
     return this.danhSachSanPham.map((sanpham, index) => {
       return (
-        <div className="col-sm-4">
-          <SanPham />
+        <div className="col-sm-4" key={index}>
+          <SanPham
+            handleDetail={this.handleDetail}
+            handleAddSP={this.handleAddSP}
+            sanPham={sanpham}
+          />
         </div>
       );
     });
@@ -91,61 +119,7 @@ export default class BaiTapGioHang extends Component {
             <div className="container">
               <div className="row">{this.renderDanhSachSanPham()}</div>
             </div>
-            <div
-              className="modal fade"
-              id="modelId"
-              tabIndex={-1}
-              role="dialog"
-              aria-labelledby="modelTitleId"
-              aria-hidden="true"
-            >
-              <div
-                className="modal-dialog"
-                role="document"
-                style={{ maxWidth: 1000 }}
-              >
-                <div className="modal-content">
-                  <div className="modal-header">
-                    <h5 className="modal-title">Giỏ hàng</h5>
-                    <button
-                      type="button"
-                      className="close"
-                      data-dismiss="modal"
-                      aria-label="Close"
-                    >
-                      <span aria-hidden="true">×</span>
-                    </button>
-                  </div>
-                  <div className="modal-body">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th>Mã sản phẩm</th>
-                          <th>tên sản phẩm</th>
-                          <th>hình ảnh</th>
-                          <th>số lượng</th>
-                          <th>đơn giá</th>
-                          <th>thành tiền</th>
-                        </tr>
-                      </thead>
-                      <tbody />
-                    </table>
-                  </div>
-                  <div className="modal-footer">
-                    <button
-                      type="button"
-                      className="btn btn-secondary"
-                      data-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" className="btn btn-primary">
-                      Save
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Modal danhSachGioHang={this.state.danhSachGioHang} />
             <div className="row">
               <div className="col-sm-5">
                 <img
